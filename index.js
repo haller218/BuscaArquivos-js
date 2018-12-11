21a588c297918464e7f348a491a39a26e3ba35c7
Eliseu Lucena Barros <eliseu42lucena@gmail.com>
2018-12-11 20:28:49 -0300

    new research data for pathern in file lines

--- index.js
+++ index.js
@@ -2,26 +2,90 @@
 
 const fs = require("fs")
 
-console.log("Hello, World!")
+// regex de busca principal
+const reg = /23[\s\t\.\-]?595[\s\t\.\-]?762[\s\t\.\-]?/
 
-console.log(fs.readdirSync('.'))
+let recursiveSearchFileByPath = (path) => {
 
-let reg = /\s(OR)\s/g
+    path = path.replace(/^/, './')
 
-let file = fs.readFileSync('LICENSE').toString()
+    let dll = /^.*.dll$/
 
-// console.log(file);
+    let count = 0
 
-matchs = file.match(/\s(OR)\s?\n?/);
+    let files = []
 
-// console.log(matchs[0]);
+    let flag = false
+
+    let lines = 0
+
+    let superArray = []
+
+    fs.readFileSync(path)
+        .toString()
+        .split('\n')
+        .forEach(r => {
+            lines++
+            if (r.match(reg) && !(r.match(dll))) {
+
+                filepath = (fs.realpathSync(path).replace(/\\/g, "\/"))
+                files.push({ path: filepath, lines: lines });
+                superArray.push({ "Arquivo": filepath });
+                superArray.push({ "Encontrado": r.match(reg)[0] })
+                superArray.push({ "Linhas": lines })
+                superArray.push({ "Linha": r.match(reg).input.substring(0, 160) })
+                // files[filepath] = lines
+                flag = true
+                count++
+            }
+        })
+
+    if (flag) {
+
+        superArray.push(count);
+        superArray.push(files);
+        return superArray
+    }
 
 
-return 0;
-let result = reg.exec(file);
+}
 
-console.log(result);
+let searchDirectoryRecursive = (pathroot) => {
 
-console.log(result.input.substring(result.index, (result.index+20)));
+    pathroot = pathroot.replace(/^/, './')
 
+    func = pah => {
+        fs.readdirSync(pah)
+            .map(r => {
 
+                // mostra o caminho de todos os arquivos
+                // console.log(fs.realpathSync(pah+"/"+r));
+
+                if ((fs.statSync(pah + "/" + r)).isDirectory())
+                    func(pah + "/" + r)
+                else {
+                    if ((fs.statSync(pah + "/" + r)).isFile()) {
+
+                        let result = recursiveSearchFileByPath(pah + "/" + r)
+                        if (!!result)
+                            console.log(result)
+                    }
+                }
+            })
+    }
+
+    func(pathroot)
+}
+
+let main = () => {
+
+    if (!!global.process.argv[2])
+        searchDirectoryRecursive(global.process.argv[2])
+    else
+        searchDirectoryRecursive(".")
+    console.log("");
+    console.log("Fim");
+    return 0
+}
+
+main()
